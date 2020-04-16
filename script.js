2baaf9fd0168a69954ca1a492f63363cd075e52f
odesystem <rolandosystem2014@gmail.com>
2020-04-16 13:49:55 -0400

    c5

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
-}
+
+window.addEventListener("load", function(event) {
+    
+   if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('./sw.js')
+               .then(reg => console.log('Registro de SW exitoso', reg))
+               .catch(err => console.warn('Error al tratar de registrar el sw', err))
+   }	
+
+   document.getElementById("boton1").addEventListener("click", function(){
+       alert('Prueba !!!');
+	   window.document.title = 'Hola!';
+   });
+	
+});
+
+
+
+
+
+
+
