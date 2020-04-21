dbbeeddafbb65de37250fd985819b6456da98459
odesystem <rolandosystem2014@gmail.com>
2020-04-21 15:50:14 -0400

    otro1

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 
 window.addEventListener("load", function(event) {
-    
+   var w_ingreso=0;
    if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./sw.js')
-               .then(reg => alert('Registro de SW exitoso', reg))
+               .then(reg => w_ingreso=1)
                .catch(err => alert('Error al tratar de registrar el sw', err))
    }	
 
-   /*document.getElementById("boton1").addEventListener("click", function(){
-       //alert('Prueba !!!');
-	   document.getElementById("h_2").innerText = 'Hola !!!';
-   });*/
+   document.getElementById("boton1").addEventListener("click", function(){
+	   alert('Entro 3');
+       alert(TipoVentana());
+	   //document.getElementById("h_2").innerText = 'Hola !!!';
+   });
+   
+   
+   function TipoVentana() { 
+       return navigator.standalone || (window.matchMedia('(display-mode: standalone)').matches); 
+   }
 	
 });
 
