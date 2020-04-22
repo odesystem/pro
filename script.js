a14caaa423d5532f00ed73aa439e049f6b8d86c3
odesystem <rolandosystem2014@gmail.com>
2020-04-22 19:51:02 -0400

    otro1

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+const divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
 
-window.addEventListener("load", function(event) {
-   var w_ingreso=0;
-   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js')
-               .then(reg => w_ingreso=1)
-               .catch(err => alert('Error al tratar de registrar el sw', err))
-   }	
+/* Put code here */
 
-   document.getElementById("boton1").addEventListener("click", function(){
-	   alert('Entro 3');
-       alert(TipoVentana());
-	   //document.getElementById("h_2").innerText = 'Hola !!!';
-   });
-   
-   
-   function TipoVentana() { 
-       return navigator.standalone || (window.matchMedia('(display-mode: standalone)').matches); 
-   }
-	
-});
 
+
+/* Only register a service worker if it's supported */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js');
+}
+
+/**
+ * Warn the page must be served over HTTPS
+ * The `beforeinstallprompt` event won't fire if the page is served over HTTP.
+ * Installability requires a service worker with a fetch event handler, and
+ * if the page isn't served over HTTPS, the service worker won't load.
+ */
+/*if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS');
+  const link = requireHTTPS.querySelector('a');
+  link.href = window.location.href.replace('http://', 'https://');
+  requireHTTPS.classList.remove('hidden');
+}*/
